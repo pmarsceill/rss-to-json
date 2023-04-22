176b58496932e4913cb34ad001fbabfa219533c1
Patrick Marsceill <patrick@thismodernweb.com>
2023-04-22 10:02:56 +0000

    testing

--- test.mjs
+++ test.mjs
@@ -4,7 +4,9 @@ import pkg from './dist/index.js';
 const { parse } = pkg;
 
 (async () => {
-    var rss = await parse('https://feeds.washingtonpost.com/rss/business/technology');
+    var rss = await parse('https://rss.app/feeds/2H9AstQcAnoJ5RnJ.xml');
+    // var rss = await parse('https://www.geekwire.com/feed');
+    // var rss = await parse('https://feeds.washingtonpost.com/rss/business/technology');
     // var rss = await parse('https://newsletterss.com/rss/mauricio');
     // var rss = await parse('https://medium.com/feed/@extropy-io');
     // var rss = await parse('https://blog.ethereum.org/feed.xml');
